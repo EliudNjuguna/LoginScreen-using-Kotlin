ca7305d78c87bbc562317005527dce0d35888d3e
Eliud Njuguna Kamau <njugunael@gmail.com>
2017-11-30 00:44:20 +0300

    initial commit
    Login Screen Kotlin

---
+++ app/src/main/java/rhynix/eliud/loginscreen/MainActivity.kt
@@ -0,0 +1,17 @@
+package rhynix.eliud.loginscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setBackgroundDrawableResource(R.drawable.back)
+        this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+
+    }
+}
